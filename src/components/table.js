a90a0bcf38419a06658a530a255b7169977411c1
Grigoriy Semenov <grig.semenov96@ya.ru>
2026-06-19 15:59:39 +0300

    finish smart-table

--- src/components/table.js
+++ src/components/table.js
@@ -8,16 +8,47 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
+    const {tableTemplate, rowTemplate, before = [], after = []} = settings;
     const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    [...before].reverse().forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.prepend(root[subName].container);
+    });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+    after.forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.append(root[subName].container);
+    });
+
+    root.container.addEventListener('change', () => {
+        onAction();
+    });
+
+    root.container.addEventListener('reset', () => {
+        setTimeout(() => onAction());
+    });
+
+    root.container.addEventListener('submit', (event) => {
+        event.preventDefault();
+        onAction(event.submitter);
+    });
 
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+
+            Object.keys(item).forEach(key => {
+                const element = row.elements[key];
+
+                if (element) {
+                    element.textContent = item[key];
+                }
+            });
+
+            return row.container;
+        });
+
         root.elements.rows.replaceChildren(...nextRows);
     }
 
